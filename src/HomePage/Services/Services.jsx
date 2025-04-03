4160927a6490744de85babbd5c84286d97795fac
unknown <arafat.innotimes.net@gmail.com>
2025-04-03 21:44:25 +0600

    Service route

--- src/HomePage/Services/Services.jsx
+++ src/HomePage/Services/Services.jsx
@@ -1,23 +1,13 @@
-"use client"
+
 import axios from "axios";
-import { useEffect, useState } from "react";
 import ServiceCard from "./ServicesCard";
-
+const getService = async () => {
+    const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/services`)
+    return response.data;
+}
 const Services = () => {
-    const [services, setServices] = useState([]);
+    const services = getService()
     console.log(services);
-    useEffect(() => {
-        const fetchServices = async () => {
-            try {
-                const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/services`);
-                setServices(response.data.data); // Assuming the API response structure
-            } catch (error) {
-                console.error("Error fetching services:", error);
-            }
-        };
-        fetchServices();
-    }, []);
-
     return (
         <div className="mt-4 container mx-auto">
             <div className="text-center flex flex-col justify-center gap-2 items-center mb-10">
@@ -28,11 +18,13 @@ const Services = () => {
                     words which do not look even slightly believable.
                 </p>
             </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {services.slice(0, 6).map(service => (
-                    <ServiceCard key={service._id} service={service} />
-                ))}
-            </div>
+            {
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {services.map(service => (
+                        <ServiceCard key={service._id} service={service} />
+                    ))}
+                </div>
+            }
         </div>
     );
 };
